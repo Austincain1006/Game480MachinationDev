f17a987f7b7f5bc2580cd8cf6d39cac913552bab
Austin <133702741+Austincain1006@users.noreply.github.com>
2023-09-03 16:52:54 -0400

    Added Player Collisions to TileMap

--- Machination/Assets/Scripts/CharacterMovement.cs
+++ Machination/Assets/Scripts/CharacterMovement.cs
@@ -5,16 +5,12 @@ using UnityEngine.UIElements;
 
 /**
  * Script that controls the movement of the player
- * 
  */
 public class PlayerScript : MonoBehaviour
 {
-    public GameObject player;
-    public float moveSpeed;
-
-    //private Vector2 moveInput;
-    public Rigidbody2D rb;
-
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    private Vector2 movement;
 
     void Start()
     {
@@ -23,35 +19,24 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        // Player Left/Right Movement
-        if (Input.GetKey(KeyCode.D))
-        { 
-            player.transform.position += new Vector3( moveSpeed, 0, 0 ) * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            player.transform.position += new Vector3( moveSpeed * -1.0f, 0, 0 ) * Time.deltaTime;
-        }
+        // Store Player Input from A/D and W/S Keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        // Player Up/Down Movement
-        if (Input.GetKey(KeyCode.W))
-        {
-            player.transform.position += new Vector3( 0, moveSpeed, 0 ) * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            player.transform.position += new Vector3( 0, moveSpeed * -1.0f, 0 ) * Time.deltaTime;
-        }
+        // Apply Input to Movement Function
+        movePlayer(horizontalInput, verticalInput);
 
     }
-
+    
+    // Update Player Pos per Physics Frame
     public void FixedUpdate()
     {
-
+        rb.MovePosition( rb.position + movement * moveSpeed * Time.fixedDeltaTime );
     }
 
-    private void movePlayer(Vector2 movement)
+    // Set Movement Vector from Player Input
+    private void movePlayer(float h, float v)
     {
-        
+        movement = new Vector2(h, v);
     }
 }
