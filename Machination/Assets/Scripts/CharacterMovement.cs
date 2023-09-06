c119f4e7ec7877bd03c7635a266c96890c9b79c4
Colin Barry <colin.andrew.barry@gmail.com>
2023-09-06 14:54:04 -0400

    fix diagonal movement speed

--- Machination/Assets/Scripts/CharacterMovement.cs
+++ Machination/Assets/Scripts/CharacterMovement.cs
@@ -8,35 +8,35 @@ using UnityEngine.UIElements;
  */
 public class PlayerScript : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed;
-    private Vector2 movement;
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private float moveSpeed;
+	private Vector2 movement;
 
-    void Start()
-    {
+	void Start()
+	{
 
-    }
+	}
 
-    void Update()
-    {
-        // Store Player Input from A/D and W/S Keys
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+	void Update()
+	{
+		// Store Player Input from A/D and W/S Keys
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
 
-        // Apply Input to Movement Function
-        movePlayer(horizontalInput, verticalInput);
+		// Apply Input to Movement Function
+		movePlayer(horizontalInput, verticalInput);
 
-    }
-    
-    // Update Player Pos per Physics Frame
-    public void FixedUpdate()
-    {
-        rb.MovePosition( rb.position + movement * moveSpeed * Time.fixedDeltaTime );
-    }
+	}
 
-    // Set Movement Vector from Player Input
-    private void movePlayer(float h, float v)
-    {
-        movement = new Vector2(h, v);
-    }
+	// Update Player Pos per Physics Frame
+	public void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+
+	// Set Movement Vector from Player Input
+	private void movePlayer(float h, float v)
+	{
+		movement = Vector2.ClampMagnitude(new Vector2(h, v), 1);
+	}
 }
