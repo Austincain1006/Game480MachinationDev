d293c28f34397014592763b994aec6ae0a8ac5bc
dms7281 <93952081+dms7281@users.noreply.github.com>
2023-09-08 12:51:40 -0400

    -

--- Machination/Assets/Scripts/Item_Interact_Recieve_Destroy.cs
+++ Machination/Assets/Scripts/Item_Interact_Recieve_Destroy.cs
@@ -12,6 +12,8 @@ public class Item_Interact_Recieve_Destroy : MonoBehaviour
     //Collider2D collision;
     public SpriteRenderer treeTexture;
 
+    //public GameObjectC treeTexture;
+
 
 
     // Start is called before the first frame update
@@ -19,7 +21,7 @@ public class Item_Interact_Recieve_Destroy : MonoBehaviour
     {
         itemBounds = gameObject.GetComponent<BoxCollider2D>();
         //collision = gameObject.GetComponent<BoxCollider2D>();
-        treeTexture = gameObject.GetComponent<SpriteRenderer>();
+        treeTexture = gameObject.GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -38,9 +40,12 @@ public class Item_Interact_Recieve_Destroy : MonoBehaviour
 
             if ( Input.GetKeyDown(KeyCode.E) ){
                 Debug.Log("Amogus with swag");
-                //treeTexture.transform.position = new Vector3(0, 0,)
+                //treeTexture.transform.SetLocalPositionAndRotation(new UnityEngine.Vector3(0,0,0), new UnityEngine.Quaternion(0f,0f,0f,1f));
+                
                 //treeTexture.transform.RotateAround(itemBounds.transform.position, , 20* Time.deltaTime);
-                Destroy(gameObject);
+                treeTexture.transform.Rotate(0,0,-90);
+                treeTexture.transform.position = new UnityEngine.Vector3(0f,0f,/**-(treeTexture.size.y/2)**/ 0f);
+                //Destroy(gameObject);
                 
             }
         }
