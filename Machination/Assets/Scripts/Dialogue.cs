ebb1d2c092af6eb1442c4abaa254aa9474c42538
Austin <133702741+Austincain1006@users.noreply.github.com>
2023-09-10 15:27:03 -0400

    Prototype Dialogue System

---
+++ Machination/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,65 @@
+// Citation: Code heavily inspired by: https://www.youtube.com/watch?v=8oTYabhj248
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        startDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Allow player to skip typing animation through input
+        if ( Input.GetMouseButtonDown(0) )
+        {
+            if ( textComponent.text == lines[index])
+            {
+                nextLine();
+            } else {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void startDialogue()
+    {
+        index = 0;
+        StartCoroutine( typeLine() );
+    }
+
+    // Type out next line character by character
+    IEnumerator typeLine()
+    {
+        foreach ( char c in lines[index].ToCharArray() )
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void nextLine()
+    {
+        if ( index < lines.Length - 1 ) {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine( typeLine() );
+        } else {
+            gameObject.SetActive( false );
+        }
+
+    }
+}
