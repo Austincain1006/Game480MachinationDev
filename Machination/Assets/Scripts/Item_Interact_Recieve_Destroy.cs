5c89d420993683473602df0cf108ac5c95eab69d
dms7281 <93952081+dms7281@users.noreply.github.com>
2023-09-15 14:00:42 -0400

    uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Machination/Assets/Scripts/Item_Interact_Recieve_Destroy.cs
+++ Machination/Assets/Scripts/Item_Interact_Recieve_Destroy.cs
@@ -11,6 +11,11 @@ public class Item_Interact_Recieve_Destroy : MonoBehaviour
     public BoxCollider2D itemBounds;
     //Collider2D collision;
     public SpriteRenderer treeTexture;
+    public SpriteRenderer treeBaseTexture;
+
+    public bool isNearResource;
+
+    public bool resourceDestroyed = false;
 
     //public GameObjectC treeTexture;
 
@@ -19,36 +24,68 @@ public class Item_Interact_Recieve_Destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        itemBounds = gameObject.GetComponent<BoxCollider2D>();
+        //itemBounds = gameObject.GetComponent<BoxCollider2D>();
         //collision = gameObject.GetComponent<BoxCollider2D>();
-        treeTexture = gameObject.GetComponentInChildren<SpriteRenderer>();
+        //treeTexture = gameObject.GetComponentInChildren<SpriteRenderer>();
+
+        //treeBaseTexture = gameObject.GetComponent<SpriteRenderer>();
+
+        treeTexture.transform.position = new UnityEngine.Vector3(
+                    gameObject.transform.position.x,
+                    (treeBaseTexture.transform.localPosition.y + (treeTexture.size.y/2)),
+                    gameObject.transform.position.z);
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-
-    
-    private void OnTriggerStay2D(Collider2D collision)
-    {       
-        if ( collision.tag == "Player" )
-        {
-            //Debug.Log("Amogus");
-
-            if ( Input.GetKeyDown(KeyCode.E) ){
+        if ( Input.GetKeyDown(KeyCode.E) && isNearResource && !resourceDestroyed){
                 Debug.Log("Amogus with swag");
                 //treeTexture.transform.SetLocalPositionAndRotation(new UnityEngine.Vector3(0,0,0), new UnityEngine.Quaternion(0f,0f,0f,1f));
                 
                 //treeTexture.transform.RotateAround(itemBounds.transform.position, , 20* Time.deltaTime);
                 treeTexture.transform.Rotate(0,0,-90);
-                treeTexture.transform.position = new UnityEngine.Vector3(0f,0f,/**-(treeTexture.size.y/2)**/ 0f);
+
+                treeTexture.transform.position = new UnityEngine.Vector3(
+                    (gameObject.transform.position.x + (treeTexture.size.y/2) + (treeBaseTexture.size.x)),
+                    (gameObject.transform.position.y),
+                    gameObject.transform.position.z);
+
+                treeTexture.sortingOrder = 2;
+
+                resourceDestroyed = true;
+                
                 //Destroy(gameObject);
                 
             }
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {       
+        if ( collision.tag == "Player" )
+        {
+            Debug.Log("Amogus Entering");
+
+            isNearResource = true;
+
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {       
+        if ( collision.tag == "Player" )
+        {
+            Debug.Log("Amogus Leaving");
+
+            isNearResource = false;
+
+        }
+    }
+
+    //Maybe make new function for tree properties after it's been destroyed
     
 }
